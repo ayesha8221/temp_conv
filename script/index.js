35a82504a8bfa8587b318de45dd097633b731680
ayesha8221 <ayesha.glnt012@gmail.com>
2023-05-23 16:33:19 +0200

    temp conv

---
+++ script/index.js
@@ -0,0 +1,8 @@
+// function calculate
+
+let farenheit = document.getElementById('farenheit').value;
+let celcius = document.getElementById('celcius').value;
+
+let result= (farenheit= (celcius * 1.8) + 32) 
+document.getElementById('celcius').value= result.farenheit
+
