512c5e2e2a8bb4919b0edc4cab5f04a72283cdda
ayesha8221 <ayesha.glnt012@gmail.com>
2023-05-25 10:24:18 +0200

    func done

--- script/index.js
+++ script/index.js
@@ -1,12 +1,19 @@
 // function calculate
-function calculate(){
-let farenheit = document.getElementById('farenheit').value;
-let celsius = document.getElementById('celsius').value;
+function calcF(){
 
-// celsius
-let result1 =
+let farenheit = document.getElementById('farenheit').value;
+
+ let result1 = (farenheit * 1.8 + 32)
+ document.getElementById('onscreen').innerHTML= result1.toFixed(2);
 }
-   
+
+function calcC(){
+    let celsius = document.getElementById('farenheit').value;
+
+    let result2 = (celsius * 0.5 - 32)
+    document.getElementById('onscreen').innerHTML= result2.toFixed(2);
+}
+
 function clearInterval() {
     document.getElementById('farenheit').value= ""
     document.getElementById('celsius').value= ""
